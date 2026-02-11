f68e27d0af42dd66980a1de0747053983daf6921
jojostin27 <yostinxd148@gmail.com>
2026-02-10 23:04:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+const loading = document.getElementById("loading");
+const main = document.getElementById("main");
+const final = document.getElementById("final");
+
+const question = document.getElementById("question");
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
-const message = document.getElementById("message");
+
+let step = 0;
+
+loading.addEventListener("click", () => {
+    loading.classList.remove("active");
+    main.classList.add("active");
+    question.textContent = "¿Quieres ser mi Valentine, Ayudina (stefanie amor de mi vida)?";
+});
 
 yesBtn.addEventListener("click", () => {
-    message.textContent = "💖 Por el segundo San Valentín y muchos más 💖";
-    yesBtn.style.display = "none";
-    noBtn.style.display = "none";
+    step++;
+
+    if (step === 1) {
+        question.textContent = "¿Estás segura?";
+    } 
+    else if (step === 2) {
+        question.textContent = "¿Si aceptas vas a tener 10 hijos conmigo, estás realmente segura?";
+    } 
+    else {
+        main.classList.remove("active");
+        final.classList.add("active");
+    }
 });
 
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * 300 - 150;
     const y = Math.random() * 300 - 150;
     noBtn.style.transform = `translate(${x}px, ${y}px)`;
-    message.textContent = "😏 no te vas a librar de mí";
+
+    const frases = [
+        "no te vas a librar de mí",
+        "siempre estaremos juntos",
+        "te vas a casar conmigo"
+    ];
+
+    question.textContent = frases[Math.floor(Math.random() * frases.length)];
 });
